36a1846c9c160038115dc5730eaeb4cf41330a93
EA_Beier <39953649+EAbeier@users.noreply.github.com>
2020-03-18 19:26:55 -0300

    Create Aula1.java

---
+++ Aula1.java
@@ -0,0 +1,13 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Aula1 {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        Calculadora calcx2 = new Calculadora();
+
+        PrintStream ps;
+        ps = System.out;
+        calc.Run();
+    }
+}
